2b100f0c9ebad1ea31747ec809c7205df7aa0638
DeadPool-4422 <123509971+DeadPool-4422@users.noreply.github.com>
2023-10-01 00:43:43 +0530

    Got rid of security issues over null a link warns

--- src/Footer_img.js
+++ src/Footer_img.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FaFacebookF, FaTwitter, FaLinkedinIn, FaInstagram, FaGoogle, FaGithub } from "react-icons/fa";
+
 function Footer_Img() {
     return (
         <>
@@ -14,15 +15,15 @@ function Footer_Img() {
                         role="button"
                         data-mdb-ripple-color="dark"
                         target='_blank'
-                    ><i className="fab fa-facebook-f"><FaFacebookF /></i
-                    ></a>
+                        rel="noopener noreferrer"
+                    ><i className="fab fa-facebook-f"><FaFacebookF /></i></a>
+
                     <a
                         className="btn btn-link btn-floating btn-lg text-dark m-1"
                         href="#!"
                         role="button"
                         data-mdb-ripple-color="dark"
-                    ><i className="fab fa-twitter"><FaTwitter /></i
-                    ></a>
+                    ><i className="fab fa-twitter"><FaTwitter /></i></a>
 
                     {/* <!-- Google --> */}
                     <a
@@ -30,36 +31,35 @@ function Footer_Img() {
                         href="#!"
                         role="button"
                         data-mdb-ripple-color="dark"
-                    ><i className="fab fa-google"><FaGoogle /></i
-                    ></a>
+                    ><i className="fab fa-google"><FaGoogle /></i></a>
 
                     {/* <!-- Instagram --> */}
                     <a
                         className="btn btn-link btn-floating btn-lg text-dark m-1"
                         href="https://www.instagram.com/!"
                         role="button"
-                        target='_blank'
                         data-mdb-ripple-color="dark"
-                    ><i className="fab fa-instagram"><FaInstagram /></i
-                    ></a>
+                        target='_blank'
+                        rel="noopener noreferrer"
+                    ><i className="fab fa-instagram"><FaInstagram /></i></a>
 
                     {/* <!-- Linkedin --> */}
                     <a
                         className="btn btn-link btn-floating btn-lg text-dark m-1"
                         href="https://www.linkedin.com/feed/"
                         role="button"
-                        target='_blank'
                         data-mdb-ripple-color="dark"
-                    ><i className="fab fa-linkedin"><FaLinkedinIn /></i
-                    ></a>
+                        target='_blank'
+                        rel="noopener noreferrer"
+                    ><i className="fab fa-linkedin"><FaLinkedinIn /></i></a>
+
                     {/* <!-- Github --> */}
                     <a
                         className="btn btn-link btn-floating btn-lg text-dark m-1"
                         href="#!"
                         role="button"
                         data-mdb-ripple-color="dark"
-                    ><i className="fab fa-github"><FaGithub /></i
-                    ></a>
+                    ><i className="fab fa-github"><FaGithub /></i></a>
                 </div>
                 <div className="card-footer text-body-secondary">
                     Designed by Mohit Saxena
